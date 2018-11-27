e46788c96b0a372192bfce2c55f7086be4822775
Mike Seese <seesemichaelj@gmail.com>
2018-11-27 02:36:52 -0500

    get recognition stuff to work

--- src/recognize.ts
+++ src/recognize.ts
@@ -6,6 +6,7 @@ import * as fr from "@seesemichaelj/face-recognition";
 fr.withCv(cv);
 fr.winKillProcessOnExit();
 
+const detector = fr.FaceDetector();
 const recognizer = fr.FaceRecognizer();
 
 const expectedNumArgs = 3;
@@ -15,22 +16,41 @@ if (process.argv.length !== expectedNumArgs) {
 
 const model = process.argv[2];
 
-recognizer.load(require(model));
+recognizer.load(require(path.resolve(model)));
+console.log(recognizer.getDescriptorState());
 
-const cap = new cv.VideoCapture(1);
+const cap = new cv.VideoCapture(0);
 
 let done = false;
 while (!done) {
   const frame = cap.read();
   const rgbFrame = fr.cvImageToImageRGB(new fr.CvImage(frame));
 
-  const predictions = recognizer.predict(rgbFrame);
+  const faces = detector.locateFaces(rgbFrame);
+  const faceRects = faces.map((face) => face.rect);
+  const faceChips = detector.getFacesFromLocations(rgbFrame, faceRects);
 
-  for (let i = 0; i < predictions.length; i++) {
-    //
+  for (let i = 0; i < faceChips.length; i++) {
+    const chip = faceChips[i];
+    const rect = faceRects[i];
+
+    const left = rect.left;
+    const top = rect.top;
+    const right = rect.right;
+    const bottom = rect.bottom;
+
+    frame.drawRectangle(fr.toCvRect(rect), new cv.Vec3(0, 0, 255));
+    frame.drawRectangle(new cv.Point2(left, bottom + 35), new cv.Point2(right, bottom), new cv.Vec3(0, 0, 255), cv.FILLED);
+
+    const predictions = recognizer.predict(chip);
+    const sortedPredictions = predictions.sort((p1, p2) => p1.distance - p2.distance);
+
+    console.log(sortedPredictions);
+
+    console.log("mikeLive = [" + recognizer.getFaceDescriptors(chip) + "];");
+
+    frame.putText(sortedPredictions[0].className, new cv.Point2(left + 6, bottom + 31), cv.FONT_HERSHEY_DUPLEX, 1.0, new cv.Vec3(255, 255, 255), 1);
   }
-
-  //frame.drawRectangle(fr.toCvRect(rect), new cv.Vec3(0, 0, 255));
 
   cv.imshow('blah', frame);
   const key = cv.waitKey(100);
