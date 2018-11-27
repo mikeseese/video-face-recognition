310059909c7f5edcc19e10db23918570fff991b4
Mike Seese <seesemichaelj@gmail.com>
2018-11-27 02:37:42 -0500

    log means of classes for plotting purposes

---
+++ src/plot.ts
@@ -0,0 +1,18 @@
+import * as path from "path";
+import * as fr from "@seesemichaelj/face-recognition";
+
+const expectedNumArgs = 3;
+if (process.argv.length !== expectedNumArgs) {
+  throw new Error(`Received ${process.argv.length - 2} arguments, expected ${expectedNumArgs}`);
+}
+
+const model = require(path.resolve(process.argv[2]));
+
+for (let i = 0; i < model.length; i++) {
+  const personName: string = model[i].className;
+  const faceDescriptors = model[i].faceDescriptors.map(arr => new fr.Array(arr));
+
+  const meanDescriptors: number[] = fr.mean(faceDescriptors).getData();
+
+  console.log(personName + " = [" + meanDescriptors + "];");
+}
