ab955215d0db76522e2363df9b01316c44040b74
Mike Seese <seesemichaelj@gmail.com>
2018-11-28 01:01:45 -0500

    some folder rearch

--- src/cli/recognize.ts
+++ src/cli/recognize.ts
@@ -26,33 +26,34 @@ while (!done) {
   const frame = cap.read();
   const rgbFrame = fr.cvImageToImageRGB(new fr.CvImage(frame));
 
-  const faces = detector.locateFaces(rgbFrame);
-  const faceRects = faces.map((face) => face.rect);
-  const faceChips = detector.getFacesFromLocations(rgbFrame, faceRects);
+  // const faces = detector.locateFaces(rgbFrame);
+  // const faceRects = faces.map((face) => face.rect);
+  // const faceChips = detector.getFacesFromLocations(rgbFrame, faceRects);
+  const faceChips = detector.detectFaces(rgbFrame);
 
   for (let i = 0; i < faceChips.length; i++) {
     const chip = faceChips[i];
-    const rect = faceRects[i];
+    //const rect = faceRects[i];
 
-    const left = rect.left;
-    const top = rect.top;
-    const right = rect.right;
-    const bottom = rect.bottom;
+    // const left = rect.left;
+    // const top = rect.top;
+    // const right = rect.right;
+    // const bottom = rect.bottom;
 
-    frame.drawRectangle(fr.toCvRect(rect), new cv.Vec3(0, 0, 255));
-    frame.drawRectangle(new cv.Point2(left, bottom + 35), new cv.Point2(right, bottom), new cv.Vec3(0, 0, 255), cv.FILLED);
+    //frame.drawRectangle(fr.toCvRect(rect), new cv.Vec3(0, 0, 255));
+    //frame.drawRectangle(new cv.Point2(left, bottom + 35), new cv.Point2(right, bottom), new cv.Vec3(0, 0, 255), cv.FILLED);
 
     const predictions = recognizer.predict(chip);
     const sortedPredictions = predictions.sort((p1, p2) => p1.distance - p2.distance);
 
-    console.log(sortedPredictions);
+    console.log(sortedPredictions[0].className);
 
-    console.log("mikeLive = [" + recognizer.getFaceDescriptors(chip) + "];");
+    //console.log("mikeLive = [" + recognizer.getFaceDescriptors(chip) + "];");
 
-    frame.putText(sortedPredictions[0].className, new cv.Point2(left + 6, bottom + 31), cv.FONT_HERSHEY_DUPLEX, 1.0, new cv.Vec3(255, 255, 255), 1);
+    //frame.putText(sortedPredictions[0].className, new cv.Point2(left + 6, bottom + 31), cv.FONT_HERSHEY_DUPLEX, 1.0, new cv.Vec3(255, 255, 255), 1);
   }
 
-  cv.imshow('blah', frame);
-  const key = cv.waitKey(100);
+  //cv.imshow('blah', frame);
+  const key = cv.waitKey(5);
   done = key !== -1 && key !== 255;
 }
--- src/cli/train.ts
+++ src/cli/train.ts
@@ -7,28 +7,41 @@ fr.withCv(cv);
 fr.winKillProcessOnExit();
 
 const recognizer = fr.FaceRecognizer();
+const detector = fr.FaceDetector();
 
 const expectedNumArgs = 3;
 if (process.argv.length !== expectedNumArgs) {
   throw new Error(`Received ${process.argv.length - 2} arguments, expected ${expectedNumArgs}`);
 }
 
-const datasetLocation = process.argv[2];
+const datasetLocation = path.resolve(process.argv[2]);
 
 const classNames = fs.readdirSync(datasetLocation).filter((f) => f !== "." && f !== "..");
 
 for (let i = 0; i < classNames.length; i++) {
   const name = classNames[i];
+  const f = path.join(datasetLocation, name);
+  if (!fs.lstatSync(f).isDirectory()) {
+    continue;
+  }
   console.log(`Adding ${name}...`);
 
-  const images = fs.readdirSync(path.join(datasetLocation, name), "binary");
+  const images = fs.readdirSync(f, "binary");
 
   const faceChips = images.map((imageFileName) => {
-    return fr.loadImage(path.join(datasetLocation, name, imageFileName));
-  });
+    const image = fr.loadImage(path.join(datasetLocation, name, imageFileName));
+    const faces = detector.detectFaces(image);
+    if (faces.length > 0) {
+      return faces[0];
+    }
+    else {
+      return null;
+    }
+  }).filter(f => f !== null);
 
   recognizer.addFaces(faceChips, name);
 }
 
+console.log(recognizer.getDescriptorState());
 const modelState = recognizer.serialize();
 fs.writeFileSync(path.join(datasetLocation, "model.json"), JSON.stringify(modelState));
