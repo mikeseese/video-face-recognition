49bd9925a74a0534e4f9f61677c3a47533c8e620
Mike Seese <seesemichaelj@gmail.com>
2018-11-28 01:41:12 -0500

    start filling out the library

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -2,11 +2,32 @@ import * as fr from "@seesemichaelj/face-recognition";
 import Faceprint from "./faceprint";
 
 export default class Session {
-  constructor() {
-    //
+  private detector: fr.FaceDetector;
+  private recognizer: fr.FaceRecognizer;
+  private minFaceDetectionThreshold: number;
+
+  constructor(model: fr.FaceDescriptor[], minFaceDetectionThreshold: number = 0, useFaceLandmarks68Model = false) {
+    this.detector = fr.FaceDetector(useFaceLandmarks68Model);
+    this.recognizer = fr.FaceRecognizer();
+    this.recognizer.load(model);
+    this.minFaceDetectionThreshold = minFaceDetectionThreshold;
   }
 
   async getFaces(image: fr.ImageRGB): Promise<Faceprint[]> {
-    return [];
+    let result: Faceprint[] = [];
+
+    const faces = this.detector.locateFaces(image);
+    const faceRects = faces
+      .filter(mmodRect => mmodRect.confidence > this.minFaceDetectionThreshold)
+      .map(mmodRect => mmodRect.rect);
+    const faceChips = this.detector.getFacesFromLocations(image, faceRects);
+
+    for (let i = 0; i < faceChips.length; i++) {
+      const chip = faceChips[i];
+      const predictions = this.recognizer.predict(chip);
+      const sortedPredictions = predictions.sort((p1, p2) => p1.distance - p2.distance);
+    }
+
+    return result;
   }
 }
