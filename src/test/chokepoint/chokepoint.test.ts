6c5e46cbad887d0fa5eaa3ce7ed4947f4a725478
Mike Seese <seesemichaelj@gmail.com>
2018-11-28 01:00:43 -0500

    add initial automated testing

---
+++ src/test/chokepoint/chokepoint.test.ts
@@ -0,0 +1,22 @@
+import P1E from "./p1e";
+import P1L from "./p1l";
+import P2E from "./p2e";
+import P2L from "./p2l";
+
+describe("Chokepoint Dataset", () => {
+  describe("P1E", () => {
+    P1E();
+  });
+
+  describe("P1L", () => {
+    P1L();
+  });
+
+  describe("P2E", () => {
+    P2E();
+  });
+
+  describe("P2L", () => {
+    P2L();
+  });
+});
---
+++ src/test/chokepoint/p1e.ts
@@ -0,0 +1,79 @@
+import * as path from "path";
+import * as fs from "fs";
+import * as fr from "@seesemichaelj/face-recognition";
+import CamID from "../../lib/cam-id";
+
+const tests = (baseDirectory: string) => {
+  describe("S1", () => {
+    const subDirectory = path.join(baseDirectory, "S1");
+    it.skip("should run P1E S1 C1", async () => {
+      // being used as training data
+    });
+
+    it.only("should run P1E S1 C2", async () => {
+      const testDirectory = path.join(subDirectory, "C2");
+      const imageInfoFile = path.join(testDirectory, "all_file.txt");
+      const imageInfoFileDetails = fs.readFileSync(imageInfoFile, "utf8");
+      const files = imageInfoFileDetails.split("\n").filter(line => line !== "");
+
+      const session = new CamID.Session();
+      for (let i = 0; i < files.length; i++) {
+        // load image
+        const rgbImage = fr.loadImage(path.join(testDirectory, files[i]));
+
+        // detect and identify faces
+        const faces = await session.getFaces(rgbImage);
+
+        // compare to truth
+      }
+    });
+
+    it("should run P1E S1 C3", async () => {
+      //
+    });
+  });
+
+  describe("S2", () => {
+    it("should run P1E S2 C1", async () => {
+      //
+    });
+
+    it("should run P1E S2 C2", async () => {
+      //
+    });
+
+    it("should run P1E S2 C3", async () => {
+      //
+    });
+  });
+
+  describe("S3", () => {
+    it("should run P1E S3 C1", async () => {
+      //
+    });
+
+    it("should run P1E S3 C2", async () => {
+      //
+    });
+
+    it("should run P1E S3 C3", async () => {
+      //
+    });
+  });
+
+  describe("S4", () => {
+    it("should run P1E S4 C1", async () => {
+      //
+    });
+
+    it("should run P1E S4 C2", async () => {
+      //
+    });
+
+    it("should run P1E S4 C3", async () => {
+      //
+    });
+  });
+};
+
+export default tests;
