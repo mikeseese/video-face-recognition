16f31a7a76efed33f09cca9ba30900aa15f37ef6
Mike Seese <seesemichaelj@gmail.com>
2019-04-28 14:22:19 -0400

    script to install services

---
+++ install-services.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+packages/core/services/install-services.sh
--- packages/core/services/install-services.sh
+++ packages/core/services/install-services.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cp -f *.service /etc/systemd/system/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cp -f ${DIR}/*.service /etc/systemd/system/
 systemctl daemon-reload
